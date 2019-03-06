ae37b1fc9eb8cc49e293f3c0bf249054de458e14
TiffanyCao <tcao00@stuy.edu>
2019-03-06 10:59:14 -0500

    started on bronze problem setup

---
+++ USACO.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class USACO{
+
+    private static int row = 0;
+    private static int col = 0;
+    private static int elevation = 0;
+    private static int nStomp = 0;
+    private static int[][] map;
+
+    public static int bronze(String filename) throws FileNotFoundException{
+      File test = new File(filename);
+      Scanner read = new Scanner(test);
+      String line = read.nextLine();
+      boolean temp = true;
+      String num = "";
+      System.out.println(line);
+      for(int i = 0; i < line.length(); i++){
+        if(line.charAt(i) == ' ') temp = false;
+        if(temp){
+          num+= line.charAt(i);
+        }
+        if(i == line.length() - 1) temp = false;
+        if(!temp && row == 0){
+          row = Integer.parseInt(num);
+          num = "";
+          temp = true;
+        }else if(!temp && col == 0){
+          col = Integer.parseInt(num);
+          num = "";
+          temp = true;
+        }else if(!temp && elevation == 0){
+          elevation = Integer.parseInt(num);
+          num = "";
+          temp = true;
+        }else if(!temp && nStomp == 0){
+          nStomp = Integer.parseInt(num);
+          num = "";
+          temp = true;
+        }
+      }
+
+      map = new int[row][col];
+      return 0;
+    }
+
+    public static void main(String[] args){
+      try{
+        bronze("makelake.1.in");
+      }catch(FileNotFoundException e){
+        System.out.println("File not found");
+      }
+    }
+}
