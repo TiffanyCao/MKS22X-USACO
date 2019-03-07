94c05c63af237ed7a560baf3344531902f51473f
Tiffany <tcao00@stuy.edu>
2019-03-06 19:22:38 -0500

    added comments

--- USACO.java
+++ USACO.java
@@ -58,24 +58,33 @@ public class USACO{
       return 0;
     }
 
+    /**A helper method that performs the stomps of the cows
+    *@param int row is the given starting row of the 3x3 cow herd
+    *@param int col is the given starting column of the 3x3 cow herd
+    *@param int level is the given amount of inches to be stomped down
+    */
     public static void stomped(int row, int col, int level){
-      int times = level;
-      while(times != 0){
+      int times = level; //records number of inches to stomp
+      while(times != 0){ //for each inch to stomp
         int largest = 0;
-        for(int i = row; i < row + 3; i++){
+        for(int i = row; i < row + 3; i++){ //check for the greatest elevation
           for(int y = col; y < col + 3; y++){
             if(map[i][y] >= largest) largest = map[i][y];
           }
         }
         for(int i = row; i < row + 3; i++){
           for(int y = col; y < col + 3; y++){
+            //if the elevation at this square is greater than or equal to the greatest elevation, stomp
             if(map[i][y] >= largest) map[i][y]--;
           }
         }
-        times--;
+        times--; //an inch has been stomped
       }
     }
 
+    /**A method used to print out the elevation map
+    *@return String
+    */
     public static String map(){
       String result = "";
       for(int i = 0; i < map.length; i++){
